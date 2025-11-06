d48dda992081613c3441a65e332f5deba207be06
Mohamed Yahia <youremail@example.com>
2025-11-06 23:16:55 +0200

    fix: use dynamic PORT for Render

--- servar.py
+++ servar.py
@@ -245,5 +245,7 @@ def kwai():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
 
